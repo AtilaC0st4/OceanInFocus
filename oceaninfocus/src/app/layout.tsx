e6a1fe0b67e6d5ddf13c3e08a68e43a218aa4aee
Átila Costa <atilarebolo@gmail.com>
2024-05-31 21:55:07 -0300

    base para o projeto

---
+++ oceaninfocus/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Ocean in Focus",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
